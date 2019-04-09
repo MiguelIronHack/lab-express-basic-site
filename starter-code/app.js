6c6680837a6274f0390a918c0ec2091459615110
miguelironhack <snoop_S_@hotmail.com>
2019-04-09 11:58:26 +0200

    changed to express

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
-app.use(express.static('./public'));
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-  res.send();
+  const data = {
+    message: 'hello squad',
+    markup: '<span>IronHack Module 2</span>',
+    myMood: Math.random() > 0.5 ? 'happy mood' : 'sad'
+  };
+  res.render('index.hbs', data);
 });
 
-app.get('/index', (req, res) => {
-  res.sendFile(`${__dirname}/index/home.html`);
-  res.sendFile(`${__dirname}/index/gallery.html`);
-  res.sendFile(`${__dirname}/index/about.html`);
-});
+app.use(express.static('/views'));
 
-app.listen(3003, () => {
+app.get('/home', (req, res) => {
+  res.sendFile(`${__dirname}/views/home.html`);
+});
+app.get('/gallery', (req, res) => {
+  res.sendFile(`${__dirname}/views/gallery.html`);
+});
+app.get('/about', (req, res) => {
+  res.sendFile(`${__dirname}/views/about.html`);
+});
+app.listen(3213, () => {
   console.log('your express app is running');
 });
---
+++ starter-code/views/js/galleryDOM.js
@@ -0,0 +1,7 @@
+const nextImg = document.getElementById('next-img');
+const prevImg = document.getElementById('prev-img');
+const gallery = document.getElementById('gallery');
+
+function photoGallery() {
+  gallery.style.background;
+}
